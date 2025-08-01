5f246921aacba23512d2cd69294e86d08b924947
Anderson092205 <andersonalfredoescobarmoran@gmail.com>
2025-07-31 18:33:39 -0600

    Update Vehiculo.java

--- Ejercicio1_POO/src/Vehiculo.java
+++ Ejercicio1_POO/src/Vehiculo.java
@@ -2,12 +2,19 @@
 
 
 //se crea la clase con los atributos
-//encapsulamiento
+//encapsulamiento para los atributos privados, protegidos del acceso directo externo
 public class Vehiculo{
     private String id;
     private int capacidadPasajeros;
     private float velocidadMaxima;
     private String estado;  // en servicio o fuera de servicio
+
+
+    //colores
+    final String RED = "\u001B[31m";
+    final String RESET = "\u001B[0m";
+
+
 
     public Vehiculo(String id, int capacidad, float velocidad, String estado) {
 
@@ -34,19 +41,27 @@ public class Vehiculo{
 
     public void setCapacidadPasajeros(int capacidad) {
 
-        if(capacidad > 0) this.capacidadPasajeros = capacidad;
+        if(capacidad > 0) {
+            this.capacidadPasajeros = capacidad;
+        }
+
+        else{
+            System.out.println(RED + "\n La capacidad de pasajeros debe ser mayor que cero." + RESET);
+        }
     }
+
 
     public void setVelocidadMaxima(float velocidad) {
         if(velocidad >= 0) {
             this.velocidadMaxima = velocidad;
+
         } else{
-            throw new IllegalArgumentException("Solo Valores positivos");
+            throw new IllegalArgumentException(RED + "\n Solo Valores positivos" + RESET);
         }
     }
 
     public void setEstado(String estado) {
-        if (estado.equals("En servicio") || estado.equals("Fuera de servicio")) {
+        if (estado.equals("En servicio") || estado.equals( "Fuera de servicio")) {
             this.estado = estado;
         }
     }
